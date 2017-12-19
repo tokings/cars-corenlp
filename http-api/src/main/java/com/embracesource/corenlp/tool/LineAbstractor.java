89d59d7bee348100a0e37b032a3bb780ade1b378
tokings <296619007@qq.com>
2017-12-19 18:08:18 +0800

    modify

--- http-api/src/main/java/com/embracesource/corenlp/tool/LineAbstractor.java
+++ http-api/src/main/java/com/embracesource/corenlp/tool/LineAbstractor.java
@@ -26,7 +26,7 @@ import edu.stanford.nlp.util.CoreMap;
  * @date	2017年11月28日 下午2:46:54
  * @copyright	http://www.embracesource.com
  */
-public class LineAbstracter {
+public class LineAbstractor {
 	
 	static final StanfordCoreNLP CORE_NLP = new StanfordCoreNLP("StanfordCoreNLP-chinese.properties");
 	static final Pattern LINE_PTN = Pattern.compile("([^x00-xff]+线)");
